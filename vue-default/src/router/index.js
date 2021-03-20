c67e1c1f15b1c948744b2bb594f677a47e7fe701
manbalboy <manbalboy@hanmail.net>
2021-03-20 14:53:07 +0900

    v-html binding

--- vue-default/src/router/index.js
+++ vue-default/src/router/index.js
@@ -1,37 +1,44 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
-import Basic from '../views/Basic.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import Home from '../views/Home.vue';
+import Basic from '../views/Basic.vue';
+import DataBindingHtml from '../views/DataBindingHtml.vue';
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/basic',
-    name: 'Basic',
-    component: Basic
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  },
+    {
+        path: '/',
+        name: 'Home',
+        component: Home,
+    },
+    {
+        path: '/basic',
+        name: 'Basic',
+        component: Basic,
+    },
+    {
+        path: '/dataBindingHtml',
+        name: 'DataBindingHtml',
+        component: DataBindingHtml,
+    },
+    {
+        path: '/about',
+        name: 'About',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    },
 
-  {
-    path: '/contact',
-    name: 'Contact',
-    component: () => import(/* webpackChunkName: "contact" , webpackPrefetch:true*/ '../views/Contact.vue')
-  }
-]
+    {
+        path: '/contact',
+        name: 'Contact',
+        component: () =>
+            import(/* webpackChunkName: "contact" , webpackPrefetch:true*/ '../views/Contact.vue'),
+    },
+];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+    history: createWebHistory(process.env.BASE_URL),
+    routes,
+});
 
-export default router
+export default router;
---
+++ vue-default/src/views/DataBindingHtml.vue
@@ -0,0 +1,15 @@
+<template>
+    <div v-html="htmlString"></div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                htmlString: '<p style="color:red;">test</p>',
+            };
+        },
+    };
+</script>
+
+<style></style>
