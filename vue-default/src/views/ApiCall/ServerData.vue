365ef44ef0f93247bdf0819beadba758c072ab0a
manbalboy <manbalboy@hanmail.net>
2021-03-20 21:27:03 +0900

    api

--- vue-default/src/views/ApiCall/ServerData.vue
+++ vue-default/src/views/ApiCall/ServerData.vue
@@ -1,12 +1,38 @@
 <template>
     <div>
         <button @click="getProductList">가져오기</button>
-
-        {{ productList }}
+        <table>
+            <thead>
+                <tr>
+                    <th>상품평</th>
+                    <th>가격</th>
+                    <th>배송비</th>
+                    <th>카테고리</th>
+                </tr>
+            </thead>
+            <tbody v-if="!productList.length">
+                <tr>
+                    <td colspan="4">조회된건이없습니다.</td>
+                </tr>
+            </tbody>
+            <tbody v-else>
+                <tr v-for="(product, index) in productList" :key="index">
+                    <td>{{ product.product_name }}</td>
+                    <td>{{ product.price }}</td>
+                    <td>{{ product.delivery_price }}</td>
+                    <td>{{ product.category }}</td>
+                    <td></td>
+                </tr>
+            </tbody>
+        </table>
+        <!-- {{ productList }} -->
     </div>
 </template>
 
 <script>
+    /**
+     *
+     */
     import axios from 'axios';
     export default {
         data() {
