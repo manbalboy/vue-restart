18f7a13693a8947a25e6d99211f0c3d33991a909
manbalboy <manbalboy@hanmail.net>
2021-03-20 23:26:21 +0900

    컴포넌트간 props 다양한 유형의 타입 내려주기

---
+++ vue-default/src/views/Component/ChildComponent.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <div>likes : {{ likes }}</div>
+        <div>isOk : {{ isOk }}</div>
+        <div>commentArray : {{ commentArray }}</div>
+        <div>author : {{ author }}</div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            likes: {
+                type: Number,
+                default: 0,
+            },
+            isOk: {
+                type: Boolean,
+                default: false,
+            },
+            commentArray: {
+                type: Array,
+            },
+            author: {
+                type: Object,
+            },
+        },
+    };
+</script>
+
+<style></style>
--- vue-default/src/views/Component/Example4.vue
+++ vue-default/src/views/Component/Example4.vue
@@ -1,17 +1,23 @@
 <template>
     <PageTitle :title="title"></PageTitle>
+    <ChildComponent :likes="likes" :isOk="isOk"></ChildComponent>
+    <ChildComponent :commentArray="[1, 2, 3, 4]" :author="{ test: 1 }"></ChildComponent>
 </template>
 
 <script>
     import PageTitle from '../../components/PageTitle.vue';
+    import ChildComponent from './ChildComponent.vue';
     export default {
         data() {
             return {
                 title: '컴포넌트 타이틀',
+                likes: 23111,
+                isOk: false,
             };
         },
         components: {
             PageTitle,
+            ChildComponent,
         },
     };
 </script>
